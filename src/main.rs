6ffb4ac428f4c7ca2aa7c01748c8a092c6d52b32
padrition <padrition@gmail.com>
2021-03-18 18:12:46 +0600

    added user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    //(1)ask for the length 
+    let mut pass_leng_str = String::new();
+
+    println!("Enter the length of wannable password: ");
+
+    io::stdin()
+        .read_line(&mut pass_leng_str)
+        .expect("Faild to read the line");
+
+    let pass_leng_str = pass_leng_str.trim();
+    
+    match pass_leng_str.parse::<u32>(){
+        Ok(i) => println!("{} is your password", i),
+        Err(..) => println!("Not a valid integer!"),
+    }
 }

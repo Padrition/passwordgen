072dd541ee662b12dd0bc4c064ec29c09e581b8a
padrition <padrition@gmail.com>
2021-03-19 17:54:08 +0600

    generate passwords

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,6 @@ mod password;
 
 fn main() {
 
-    println!("{}", password::rand_pass(16));
-
-    let mut password_codes : Vec<char> = vec![];
-
-    for i in 0..26{
-        let letter : u8 = 65 + i;
-        let c = letter as char;
-        password_codes.push(c);
-        let letter : u8 = 97 + i;
-        let c = letter as char;
-        password_codes.push(c);
-    }
-
-    println!("{:?}", password_codes);
-
     //(1)ask for the length 
     let length : u32;
 
@@ -45,6 +30,6 @@ fn main() {
         }
     }
     //(2) make a vector of chars and instert a random value to it
-
+    println!("{}", password::rand_pass(length));
 
 }

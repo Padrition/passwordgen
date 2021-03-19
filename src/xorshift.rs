e2b94bccec7971b48eebb0e50f246c7a3faa436c
padrition <padrition@gmail.com>
2021-03-19 15:27:07 +0600

    added forgoten XORs )

--- src/xorshift.rs
+++ src/xorshift.rs
@@ -5,10 +5,11 @@ pub fn get_rand() -> u32{
     let now = SystemTime::now();
     let from_unix = now.duration_since(UNIX_EPOCH).expect("Congrats on time travel!");
     let seed = from_unix.as_micros();
-    let x = seed << 1;
-    let x = x >>11;
-    let x = x <<13;
-    return_last_digits_from_u128(x, 3)
+    let x = seed;
+    let x = x ^ seed << 13;
+    let x = x ^ x >>17;
+    let x = x ^ x <<5;
+    return_last_digits_from_u128(x, 2)
 }
 
 fn return_last_digits_from_u128 (x : u128 ,len : usize) -> u32{
